2a8ab8a8862c50a66ce1916e592b7c184cea125d
minierolls <minierolls@gmail.com>
2020-08-26 14:52:02 -0700

    <feat> Initial partial Style implementation

--- src/view/style.zig
+++ src/view/style.zig
@@ -3,7 +3,11 @@
 // SPDX-License-Identifier: MIT
 // This file is part of the `termcon` project under the MIT license.
 
-pub const Color = struct {};
+pub const Style = struct {
+    fg_color: Color,
+    bg_color: Color,
+    text_decorations: TextDecorations,
+};
 
 pub const TextDecorations = struct {
     italic: bool,
@@ -11,8 +15,45 @@ pub const TextDecorations = struct {
     underline: bool,
 };
 
-pub const Style = struct {
-    fg_color: Color,
-    bg_color: Color,
-    text_decorations: TextDecorations,
+pub const Color = union(ColorType) {
+    Default: ColorDefault,
+    Named: ColorNamed,
+    Bit24: ColorBit24,
+    True: ColorTrue,
+    // TODO: Provide conversion functions
 };
+
+pub const ColorType = enum {
+    Default,
+    Named,
+    Bit24,
+    True,
+};
+
+pub const ColorDefault = enum {
+    Background,
+    Foreground,
+    Highlight,
+    Cursor,
+    _, // TODO
+};
+
+pub const ColorNamed; // TODO
+
+pub const ColorBit24 = struct {
+    hex: u24,
+
+    pub fn initHex(hex: u24) ColorBit24 {
+        return ColorBit24{ .hex = hex };
+    }
+
+    pub fn initRGB(red: u8, green: u8, blue: u8) ColorBit24 {
+        var hex: u32 = 0;
+        hex |= blue;
+        hex |= @as(u16, green) << 8;
+        hex |= @as(u24, red) << 16;
+        return ColorBit24{ .hex = hex };
+    }
+};
+
+pub const ColorTrue; // TODO
