5829881d24031c29b9df9eeeff9e5a4fc01a808c
Luke I. Wilson <theepictoastguy666@gmail.com>
2020-08-26 20:54:17 -0500

    <feat> New files for Windows backend

---
+++ src/backend/windows/event.zig
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 John Namgung.
+
+// SPDX-License-Identifier: MIT
+// This file is part of the `termcon` project under the MIT license.
+
+const event = @import("../../event.zig");
+const mouse = event.mouse;
+const key = event.key;
+
+pub fn getMouseEvent() !?mouse.Event {
+    @compileError("Unimplemented");
+}
+
+pub fn getKeyEvent() !?key.Event {
+    @compileError("Unimplemented");
+}
